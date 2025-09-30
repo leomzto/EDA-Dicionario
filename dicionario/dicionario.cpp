4cd72f165e00d8b58dc7333d9440292df82a1c17
LeoManzato <leopmanzato@gmail.com>
2025-09-29 23:54:33 -0300

    Co-authored-by: duarteezzz <duarteezzz@users.noreply.github.com>

--- dicionario/dicionario.cpp
+++ dicionario/dicionario.cpp
@@ -1,2 +1,141 @@
 #include "dicionario.hpp"
+#include <sstream>
 
+void inserirPalavra(Dicionario* dic, string* pt, string* en)
+{
+    // Lista
+    Palavra* palavra = new Palavra(*pt, *en);
+    No* novoNo = new No(palavra);
+ 
+    if (dic->inicio == nullptr)
+        dic->inicio = dic->fim = novoNo;
+    else
+    {
+        dic->fim->prox = novoNo;
+        dic->fim = novoNo;
+    }
+
+    dic->qtd_palavras++;
+
+    // Banco de Dados
+
+}
+
+
+void removerPalavra(Dicionario* dic, string* palavra)
+{
+    // Lista
+    No* posAtual = dic->inicio;
+    No* posAnterior = nullptr;
+
+    while (posAtual != nullptr)
+    {
+        if (posAtual->info->pt == *palavra || posAtual->info->en == *palavra)
+        {
+            if (posAnterior != nullptr)
+                posAnterior->prox = posAtual->prox;
+            else
+                dic->inicio = posAtual->prox;
+
+            if (posAtual == dic->fim)
+                dic->fim = posAnterior;
+
+            delete posAtual;
+            dic->qtd_palavras--;
+            break;
+        }
+
+        posAnterior = posAtual;
+        posAtual = posAtual->prox;
+    }
+
+    // Banco de Dados
+
+
+
+}
+
+string buscarPalavra(Dicionario* dic, string* palavra)
+{
+    No* aux = dic->inicio;
+    while(aux != nullptr)
+    {
+        if(aux->info->pt == *palavra)
+            return aux->info->en;
+        if (aux->info->en == *palavra)
+            return aux->info->pt;
+        aux = aux->prox;
+    }
+    return "";
+}
+
+void exibirDicionario(Dicionario* dic)
+{
+    No* posAtual = dic->inicio;
+
+    cout << "\n-=- Dicionario (" << dic->qtd_palavras << " palavras) -=-\n";
+    while (posAtual != nullptr)
+    {
+        cout << posAtual->info->pt << " -> " << posAtual->info->en << endl;
+        posAtual = posAtual->prox;
+    }
+}
+
+void traduzir(Dicionario* dic, string *texto)
+{
+    stringstream ler(*texto);
+    string palavra;
+    cout << "Traducao:" << endl;
+
+    while (ler >> palavra)
+    {
+        string traduzida = buscarPalavra(dic, &palavra);
+        if(traduzida.empty() != true)
+            cout << traduzida << " ";
+        else
+            cout << palavra << " ";
+    }
+
+    cout << endl;
+}
+
+
+/*
+void traduzirTeste01(Dicionario* dic, string *texto)
+{
+    string palavra; 
+    cout << "Traducao:" << endl;
+
+    for (char caractere : *texto) // para caada caractere no texto
+    {
+        if (caractere == ' ') // encontrou o fim da palavra
+        {
+            if (palavra.empty() == false)
+            {
+                string traducao = buscarPalavra(dic, &palavra);
+                
+                if (traducao.empty() == false)
+                    cout << traducao << " ";
+                else
+                    cout << "[ " << palavra << " ]";
+
+                palavra.clear();
+            }
+            else
+                palavra += caractere; // avança caractere
+        }
+    }
+
+    if (palavra.empty() == false)
+    {
+        string traducao = buscarPalavra(dic, &palavra);
+        
+        if (traducao.empty() == false)
+            cout << traducao << " ";
+        else
+            cout << "[ " << palavra << " ]";
+    }
+
+    cout << endl;
+}
+*/
--- dicionario/dicionario.hpp
+++ dicionario/dicionario.hpp
@@ -1,18 +1,56 @@
 #ifndef DICIONARIO_HPP
 #define DICIONARIO_HPP
 
+#include <string>
+#include <sqlite3.h>
+#include <iostream>
+
+using namespace std;
+
 typedef struct sPalavra {
+    string pt;
+    string en;
+
+    sPalavra(string pt_br, string en_us) : pt(pt_br), en(en_us) {}; 
     
 } Palavra;
 
 typedef struct sNo {
+    Palavra* info;
+    sNo* prox;
+
+    sNo(Palavra* palavra) : info(palavra), prox(nullptr) {};
 
 } No;
 
 typedef struct sDicionario {
 
+    No* inicio;
+    No* fim;
+    int qtd_palavras;
+
+    sDicionario() : inicio(nullptr), fim(nullptr), qtd_palavras(0) {};
+
 } Dicionario;
 
+// Dicionario
+void inserirPalavra(Dicionario* dic, string*pt, string* en);
+void removerPalavra(Dicionario* dic, string* palavra);
 
+string buscarPalavra(Dicionario* dic, string* palavra);
+void exibirDicionario(Dicionario* dic);
+
+void traduzir(Dicionario* dic, string *texto);
+//void traduzirTeste01(Dicionario* dic, string *texto);
+//void traduzirTeste02(Dicionario* dic, string *texto);
+
+
+
+// Banco de Dados
+bool inicializarBanco();
+
+int carregarDB(void* lista, int argc, char* argv[], char* coluna[]);
+
+void carregarDicionario(Dicionario* dic);
 
 #endif
